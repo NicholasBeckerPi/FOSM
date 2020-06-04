5c12c69309f7cde566a451d717b0d2b8257360a4
Nicholas Becker <61593970+Nicholove@users.noreply.github.com>
2020-06-04 19:45:23 -0300

    Update FOSM.py

--- FOSM.py
+++ FOSM.py
@@ -1,8 +1,5 @@
 __author__ = 'nicholove'
 
-"FOSM script - File Organizer and Send Mail script"
-
-
 import shutil
 import os
 from datetime import date
@@ -14,86 +11,99 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
-if __name__ == '__main__':
+class Computer:
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-    def action1():
+    def path_verification(self):
+        if not os.path.exists(self.file_path):
+            print('ATENÇÃO! ESTE DIRETÓRIO NÃO EXISTE!')
+            return inputs()
+        else:
+            pass
 
-        global archive_to_send
+    # Retorna a pasta com o dia
+    @staticmethod
+    def actual_day():
+        now_day = str(date.today())
+        res = re.compile(r'-')
+        now_run = re.sub(res, '_', now_day)
+        return now_run
 
-        def dir_verification():
-            return os.path.exists('./ToBeZiped')
+    # Copia para a pasta do dia
+    def copy_files(self):
+        try:
+            shutil.copytree(src=f'./{self.file_path}', dst=f'./{Computer.actual_day()}')
+        except FileExistsError:
+            pass
 
-        directory = dir_verification()
+     # Lista os arquivos que serão copiados
+    @staticmethod
+    def list_files():
+        return list(os.listdir(f'{Computer.actual_day()}'))
 
-        def make_dir():
-            if not directory:
-                try:
-                    os.mkdir('./ToBeZiped')
-                except OSError:
-                    print('Creation error!')
-
-        make_dir()
-
-        def actual_day():
-            now_run = str(date.today())
-            res = re.compile(r'-')
-            now_run = re.sub(res, '_', now_run)
-            return now_run
-
-        day_run = actual_day()
-
-        def list_files():
-            return list(os.listdir('./ToBeZiped'))
-
-        def copy_files():
-            try:
-                shutil.copytree(src='./ToBeZiped', dst=f'./{day_run}' )
-            except FileExistsError:
-                pass
-
-        def zip_files():
-            return shutil.make_archive(f'{day_run}', 'zip', './ToBeZiped')
-
-        print(f'The following archives will be copied: \n{list_files()}')
-
-        archive_to_send = zip_files()
-        zip_files()
-        copy_files()
+    # Cria um zip da pasta do dia
+    @staticmethod
+    def zip_files():
+        return shutil.make_archive(f'{Computer.actual_day()}', 'zip', f'./{Computer.actual_day()}')
 
 
-    def send_mail():
-        # Setting user
 
-        e_user = 'your_mail@gmail.com'
-        e_receiver = 'othermail@gmail.com'
-        password = 'password123'
-        subject = 'FOSM HAS A MESSAGE!!'
-        msg_body = 'Files organized and sent by FOSM'
+class EmailSender:
+    def __init__(self, user_address, receiver_address, password, subject, body_message):
+        self.user_address = user_address
+        self.receiver_address = receiver_address
+        self.password = password
+        self.subject = subject
+        self.body_message = body_message
 
+    def setting_mail(self):  # Prepara o email para ser enviado
+        global text
         message = MIMEMultipart()
-        message['From'] = e_user
-        message['To'] = e_receiver
-        message['Subject'] = subject
-
-        message.attach(MIMEText(msg_body, 'plain'))
-
-        filename = f'{archive_to_send}'
+        message['From'] = self.user_address
+        message['To'] = self.receiver_address
+        message['Subject'] = self.subject
+        message.attach(MIMEText(self.body_message, 'plain'))
+        filename = f'{Computer.actual_day()}.zip'
         attachment = open(filename, 'rb')
         part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
+        part.set_payload(attachment.read())
         encoders.encode_base64(part)
         part.add_header('Content-Disposition', f"attachment; filename = {filename}")
         message.attach(part)
         text = message.as_string()
 
-        # Sending mail
+    def sending_mail(self):  # Envia o email
         server = sm.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
-        server.login(e_user, password)
-        server.sendmail(e_user, e_receiver, text)
+        server.login(self.user_address, self.password)
+        server.sendmail(self.user_address, self.receiver_address, text)
         server.quit()
 
 
-    action1()
-    send_mail()
+def inputs():
+    path = input('Caminho do arquivo: ')
+    c1 = Computer(path)
+    c1.path_verification()
+    user_address = input('Seu email: ')
+    receiver_address = input('Destinatário: ')
+    password = input('Sua senha: ')
+    subject = input('Assunto: ')
+    body_message = input('Texto de corpo: ')
+
+    def run():
+        c1.actual_day()
+        c1.copy_files()
+        c1.list_files()
+        c1.zip_files()
+        e1 = EmailSender(user_address, receiver_address, password, subject, body_message)
+        e1.setting_mail()
+        e1.sending_mail()
+    return run()
+
+
+if __name__ == '__main__':
+    inputs()
+
+
 
